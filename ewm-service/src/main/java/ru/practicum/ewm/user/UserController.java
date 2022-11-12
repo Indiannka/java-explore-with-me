fc1c597be01e4498bc2e0eff616ce11dbe67cade
Indiannka <indiannka@yandex.ru>
2022-11-12 17:20:49 +0300

    project scheme

---
+++ ewm-service/src/main/java/ru/practicum/ewm/user/UserController.java
@@ -0,0 +1,47 @@
+package ru.practicum.ewm.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.configs.Create;
+import ru.practicum.ewm.user.converter.UserConverter;
+import ru.practicum.ewm.user.model.dto.UserDto;
+
+import javax.validation.constraints.Min;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/admin/users")
+public class UserController {
+
+    private final UserService userService;
+    private final UserConverter userConverter;
+
+    @GetMapping
+    public Collection<UserDto> getUsers(@RequestParam (defaultValue = "0", required = false) @Min(0)  int from,
+                                        @RequestParam (defaultValue = "10", required = false) @Min(1) int size,
+                                        @RequestParam (required = false) Long[] ids) {
+        log.info("GET request: запрос пользователtq с id {}", ids);
+        return userService.getUsers(ids, from, size).stream()
+                .map(userConverter::convert)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public UserDto create(@Validated({Create.class})
+                          @RequestBody UserDto userDto) {
+        log.info("POST request: создание пользователя {}", userDto.toString());
+        return userConverter.convert(userService.create(userDto));
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable("userId") long userId) {
+        log.info("DELETE request: удаление пользователя с id {}", userId);
+        userService.delete(userId);
+    }
+}
---
+++ ewm-service/src/main/java/ru/practicum/ewm/user/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewm.user;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.ewm.user.model.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Page<User> findAllByIdIn(Collection<Long> id, Pageable pageable);
+}
